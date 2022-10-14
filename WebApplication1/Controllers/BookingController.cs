6924301e0659e47ce0981aa0be0f41ca89e9eec7
zemlianin <detulie@mail.ru>
2022-10-15 01:48:23 +0300

    Add-booking

---
+++ WebApplication1/Controllers/BookingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [EnableCors("_myAllowSpecificOrigins")]
+    [Route("[controller]")]
+    public class BookingController : ControllerBase
+    {
+        [EnableCors("_myAllowSpecificOrigins")]
+
+        [HttpGet("post")]
+        public int Post(int a)
+        {
+            return a;
+        }
+        [EnableCors("_myAllowSpecificOrigins")]
+
+        [HttpGet("Booking")]
+        public int Get()
+        {
+            return 123;
+        }
+    }
+}

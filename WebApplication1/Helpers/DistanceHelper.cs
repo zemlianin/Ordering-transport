da52e07545432ad5abf1535de206af701d6bf687
lueFlake <uselessal123@gmail.com>
2022-10-16 01:34:50 +0300

    added distance helper

---
+++ WebApplication1/Helpers/DistanceHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebApplication1.Models;
+
+namespace WebApplication1.Helpers
+{
+    public class DistanceHelper
+    {
+        private static double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180;
+        }
+
+        public static double DistanceInKmBetweenEarthCoordinates(Location first, Location second)
+        {
+            var earthRadiusKm = 6371;
+
+            var dLat = DegreesToRadians(second.X - first.X);
+            var dLon = DegreesToRadians(second.Y - second.Y);
+
+            first.X = DegreesToRadians(first.X);
+            second.X = DegreesToRadians(second.X);
+
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                    Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(first.X) * Math.Cos(second.X);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return earthRadiusKm * c;
+        }
+    }
+}

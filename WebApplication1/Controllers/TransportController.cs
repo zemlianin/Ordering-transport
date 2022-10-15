622556efb75d09dce738c8150d663c83edddfc84
zemlianin <detulie@mail.ru>
2022-10-15 04:21:00 +0300

    edit-transport

--- WebApplication1/Controllers/TransportController.cs
+++ WebApplication1/Controllers/TransportController.cs
@@ -10,14 +10,14 @@ namespace WebApplication1.Controllers
     public class TransportController : Controller
     {
         [EnableCors("_myAllowSpecificOrigins")]
-
         [HttpPost("post")]
         public IActionResult Post()
         {
             return Ok();
         }
 
-        [HttpPost("get")]
+        [EnableCors("_myAllowSpecificOrigins")]
+        [HttpGet("get")]
         public Transport[] Get()
         {
             return new Transport[] {new Transport() { DriverId = 1, Id = 1, Type = "tractor"}, new Transport() { DriverId = 2, Id = 2, Type = "tractor" } };

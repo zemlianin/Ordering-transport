5368fa73bba781c21bb0993f344258dac38619b0
zemlianin <detulie@mail.ru>
2022-10-16 07:39:56 +0300

    hard-code-yyy

--- WebApplication1/Controllers/CustomerController.cs
+++ WebApplication1/Controllers/CustomerController.cs
@@ -22,9 +22,21 @@ namespace WebApplication1.Controllers
 
                 using (ApplicationContext db = new ApplicationContext())
                 {
-                    var CustomerId = db.Customers.First(a => userName == a.UserName).Id;
-                    var t = db.Forms.Where((p) => p.CustomerId == CustomerId).ToList();
-                    return JsonSerializer.Serialize(t);
+                    var Customer = db.Customers.First(a => userName == a.UserName);
+                    var t = db.Forms.Where((p) => p.CustomerId == Customer.Id).ToList();
+                    string s = "[";
+                    for (int i = 0; i < t.Count(); i++)
+                    {
+                        s += $"{{\"name\": \"{userName}\",\"lastName\": \"---\",\"telephone\": \"{Customer.PhoneNumber}\",\"beginDate\": \"{t[i].BeginDate}\",\"\"endDate\": \"{t[i].EndDate}\"," +
+                            $"\"address\": \"РоссияZZZ, Архангельская область\",\"typeOfVehicle\": \"{t[i].TransportType},\",\r\n\"priority\": \"{t[i].Priority}\",\r\n\"comment\": \"{t[i].Comment}\"}},";
+                        /*s+= JsonSerializer.Serialize<>(new() {name = userName, lastName = "--", telephone = Customer.PhoneNumber, beginDate = t[i].BeginDate, endDate = t[i].EndDate, 
+                        address = "РоссияZZZ, Архангельская область",
+                            typeOfVehicle = t[i].TransportType,
+                            priority = t[i].Priority, comment = t[i].Comment
+                        });*/
+                    }
+                    s += "]";
+                    return s;
                 }
             }
             catch (Exception ex)

df5c8a9fb2b14f2c7b9ec132fbdbef6349fe89d2
lueFlake <uselessal123@gmail.com>
2022-10-16 08:33:31 +0300

    forms service added again

--- WebApplication1/Services/FormsService.cs
+++ WebApplication1/Services/FormsService.cs
@@ -1,9 +1,30 @@
 ﻿using WebApplication1.Models;
+using WebApplication1.Helpers;
+using WebApplication1.Models;
 
 namespace WebApplication1.Services
 {
     public class FormsService
     {
-        
+        public double Range { get; }
+
+         public FormsService(double range = 10.0)
+         {
+             Range = range;
+         }
+
+         public void Distribute(Form form)
+         {
+             using (ApplicationContext db = new ApplicationContext())
+             {
+                 var transports = db.Transport.
+                     Where(a => a.transportType == form.TransportType).
+                     Where(a => DistanceHelper.DistanceInKm(
+                         a.Driver.CurrentLocation,
+                         form.Location) < Range).
+                     ToList();
+                 // TOOD: Send to drivers
+             }
+         }
     }
 }

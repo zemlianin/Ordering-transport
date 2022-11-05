4edaa360424ddb7cb7632cb73da3b852e2d6e7bf
zemlianin <detulie@mail.ru>
2022-11-05 13:26:16 +0300

    Add-Class-Testing

---
+++ API.Tests/CustomerControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Controllers;
+
+namespace API.Tests
+{
+    public class CustomerControllerTests
+    {
+        [Fact]
+        public void GetJsonTrue()
+        {
+            // Arrange
+            CustomerController controller = new CustomerController();
+
+            // Act
+            string result = controller.Get("Anya");
+
+            // Assert
+            Assert.Equal("Hello world!", result);
+        }
+    }
+}
